2a5e8bb42c0ec5abf2d3d65a31ca5681a1b94826
Wesley Dung <ducdung17903@gmail.com>
2024-08-13 22:23:03 +0700

    13/8/2024

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import mysql from "mysql2";
+import db from "../models";
 const salt = bcrypt.genSaltSync(10);
 
 // Create the connection to database
@@ -13,27 +14,24 @@ const hashUserPassword = (userPassword) => {
   return bcrypt.hashSync(userPassword, salt);
 };
 
-const createNewUser = (email, password, username) => {
+const createNewUser = async (email, password, username) => {
   let hashPassword = hashUserPassword(password);
-  connection.query(
-    "INSERT INTO user (email, password ,username) VALUES (?,?,?)",
-    [email, hashPassword, username],
-    function (err, results, fields) {
-      if (err) {
-        console.log("Error inserting user: ", err);
-      }
-    }
-  );
+
+  try {
+    await db.User.create({
+      email: email,
+      password: hashPassword,
+      username: username,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const getUserList = () => {
+const getUserList = async () => {
   let user = [];
-  connection.query("SELECT * FROM user ", function (err, results, fields) {
-    if (err) {
-      console.log("Error inserting user: ", err);
-    }
-    console.log(results);
-  });
+  user = await db.User.findAll();
+  return user;
 };
 
 module.exports = {
