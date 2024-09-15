834d0266a10dd953122c23213d993ec7deb93bbb
Wesley Dung <ducdung17903@gmail.com>
2024-09-15 20:43:04 +0700

    15/9/2024

--- src/controller/roleController.js
+++ src/controller/roleController.js
@@ -2,24 +2,12 @@ import roleApiService from "../service/roleApiService";
 
 const readFunc = async (req, res) => {
   try {
-    if (req.query.page && req.query.limit) {
-      let page = req.query.page;
-      let limit = req.query.limit;
-
-      let data = await userApiService.getUserWithPagination(+page, +limit);
-      return res.status(200).json({
-        EM: data.EM,
-        EC: data.EC, //error code
-        DT: data.DT, //data
-      });
-    } else {
-      let data = await userApiService.getAllUsers();
-      return res.status(200).json({
-        EM: data.EM,
-        EC: data.EC, //error code
-        DT: data.DT, //data
-      });
-    }
+    let data = await roleApiService.getAllRoles();
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC, //error code
+      DT: data.DT, //data
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -48,6 +36,7 @@ const createFunc = async (req, res) => {
   }
 };
 
+//To do
 const updateFunc = async (req, res) => {
   try {
     let data = await userApiService.updateUser(req.body);
@@ -68,7 +57,7 @@ const updateFunc = async (req, res) => {
 
 const deleteFunc = async (req, res) => {
   try {
-    let data = await userApiService.deleteUser(req.body.id);
+    let data = await roleApiService.deleteRole(req.body.id);
     return res.status(200).json({
       EM: data.EM,
       EC: data.EC, //error code
--- src/service/roleApiService.js
+++ src/service/roleApiService.js
@@ -31,4 +31,40 @@ const createNewRoles = async (roles) => {
     };
   }
 };
-module.exports = { createNewRoles };
+
+const getAllRoles = async () => {
+  try {
+    let data = await db.Role.findAll({ order: [["id", "DESC"]] });
+    return {
+      EM: "Get all role success",
+      EC: 0,
+      DT: data,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      EM: "Somthing wrong with roleApiService: ",
+      EC: 1,
+      DT: [],
+    };
+  }
+};
+
+const deleteRole = async (id) => {
+  try {
+    await db.Role.destroy({ where: { id: id } });
+    return {
+      EM: "Delete role success",
+      EC: 0,
+      DT: [],
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      EM: "Something wrong with roleApiService: ",
+      EC: 1,
+      DT: [],
+    };
+  }
+};
+module.exports = { createNewRoles, getAllRoles, deleteRole };
